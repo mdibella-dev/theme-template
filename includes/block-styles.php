e4dd92c34ac14d50f93d796bd58f19b17efc40b2
Marco Di Bella <mdb@marcodibella.de>
2022-12-09 12:59:26 +0100

    Update block-styles.php

--- includes/block-styles.php
+++ includes/block-styles.php
@@ -2,8 +2,8 @@
 /**
  * Additional setup for the block styles.
  *
- * @author   Marco Di Bella
- * @package  <THEME-PACKAGE>
+ * @author  Marco Di Bella
+ * @package <THEME-PACKAGE>
  */
 
 
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) or exit;
 /**
  * Script and style modifications for the block editor.
  *
- * @since  1.0.0
+ * @since 1.0.0
  */
 
 function mdb_register_block_styles()
@@ -25,7 +25,7 @@ function mdb_register_block_styles()
         '<THEME-PREFIX>-block-styles',
         get_template_directory_uri() . '/assets/src/js/block-styles.js',    // maybe add a 'build' version?
         array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
-        0,                                                                  // maybe add theme version var here?
+        THEME_VERSION,
         true
     );
 }
