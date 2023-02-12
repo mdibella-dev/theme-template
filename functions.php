1db3d0e2dd4021d75f24c94506c42e4c0661b618
Marco Di Bella <mdb@marcodibella.de>
2023-02-12 12:55:36 +0100

    Fix theme's constants.

--- functions.php
+++ functions.php
@@ -23,13 +23,13 @@ error_reporting( E_ALL ^ E_NOTICE );
 
 /** Variables and definitions **/
 
-define( __NAMESPACE__ . "THEME_VERSION", '<THEME-VERSION>' );       // The theme's version
-define( __NAMESPACE__ . "THEME_DIR", get_template_directory() );    // The theme's directory
+define( __NAMESPACE__ . "\THEME_VERSION", '<THEME-VERSION>' );              // The theme's version
+define( __NAMESPACE__ . "\THEME_DIR", get_template_directory() . '/' );     // The theme's directory
 
 
 
 /** Include files */
 
-require_once( THEME_DIR . '/includes/setup.php' );
-require_once( THEME_DIR . '/includes/block-patterns.php' );
-require_once( THEME_DIR . '/includes/block-styles.php' );
+require_once( THEME_DIR . 'includes/setup.php' );
+require_once( THEME_DIR . 'includes/block-patterns.php' );
+require_once( THEME_DIR . 'includes/block-styles.php' );
