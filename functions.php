7771225ed716ea01cd2017fb2d4f3f7a39a88302
Marco Di Bella <mdb@marcodibella.de>
2023-11-04 16:24:25 +0100

    Fix placeholder

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ error_reporting( E_ALL ^ E_NOTICE );
 
 /** Variables and definitions **/
 
-define( __NAMESPACE__ . "\THEME_VERSION", '<THEME-VERSION>' );                // The theme's version
+define( __NAMESPACE__ . "\THEME_VERSION", 'ph_THEME-VERSION' );                // The theme's version
 define( __NAMESPACE__ . "\THEME_DIR", get_template_directory() . '/' );       // The theme's directory
 define( __NAMESPACE__ . "\THEME_URI", get_template_directory_uri() .'/' );    // The theme's uri
 
