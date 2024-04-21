4a058e83514b96be8d4c806f5c25dd1de8f3176c
Marco Di Bella <mdb@marcodibella.de>
2024-04-21 17:25:42 +0200

    Standardize script and style names

--- includes/setup.php
+++ includes/setup.php
@@ -36,7 +36,7 @@ function theme_setup() {
 
     // Adds editor styles.
     add_theme_support( 'editor-styles' );
-    add_editor_style( 'assets/build/css/style-frontend.min.css' );        // change path/name if necessary
+    add_editor_style( 'assets/build/css/frontend-style.min.css' );        // change path/name if necessary
 }
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\theme_setup' );
@@ -59,7 +59,7 @@ function theme_scripts() {
 
     wp_enqueue_style(
         'ph_THEME-PREFIX-frontend-style',
-        THEME_URI . 'assets/build/css/style-frontend.min.css',  // change path/name if necessary
+        THEME_URI . 'assets/build/css/frontend-style.min.css',          // change path/name if necessary
         [],
         THEME_VERSION
     );
@@ -67,10 +67,8 @@ function theme_scripts() {
 
     wp_enqueue_script(
         'ph_THEME-PREFIX-frontend-script',
-        THEME_URI . 'assets/build/js/frontend.min.js',          // change path/name if necessary
-        [
-            'jquery'                                            // remove if not used
-        ],
+        THEME_URI . 'assets/build/js/frontend-script.min.js',          // change path/name if necessary
+        [],
         THEME_VERSION,
         true
     );
